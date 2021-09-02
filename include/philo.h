924b3a23fc6475ecc59af9b8a6f3d80deeee563a
fmint <fmint@student.21-school.ru>
2021-09-02 14:51:52 +0300

    start project philo

---
+++ include/philo.h
@@ -0,0 +1,25 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+typedef struct every_phil
+{
+
+}	t_every_phil;
+
+typedef struct all_phil
+{
+
+}	t_all_phil_info;
+
+
+
+#endif
